fbc1d3e48aa18951ca003b274ea68dff02c273b3
Vinschers <code@a.vicentin.xyz>
2026-02-04 14:27:44 +0100

    refactor(imports): create alias files

--- src/vicentin/__init__.py
+++ src/vicentin/__init__.py
@@ -1,10 +1,5 @@
 import importlib.metadata
 
-# from . import image
-from .data_structures import *
-
-from . import deep_learning as dl
-
 try:
     __version__ = importlib.metadata.version("vicentin")
 except importlib.metadata.PackageNotFoundError:
--- src/vicentin/deep_learning/train/__init__.py
+++ src/vicentin/deep_learning/train/__init__.py
@@ -1,4 +1,6 @@
 from .GenericTrainer import GenericTrainer
 from .StandardTrainer import StandardTrainer
+from .SupervisedTrainer import SupervisedTrainer
+
 from .GANTrainer import GANTrainer
 from .VAETrainer import VAETrainer
