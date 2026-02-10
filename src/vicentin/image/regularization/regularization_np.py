fix(image-regularization): fix gradient computation

src/vicentin/image/regularization/regularization_np.py
@@ -3,10 +3,10 @@ from vicentin.image.differentiation import grad
 
 
 def tychonov(img):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return np.sum(dx**2 + dy**2)
 
 
 def total_variation(img, epsilon=1e-2):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return np.sum(np.sqrt(epsilon**2 + dx**2 + dy**2))
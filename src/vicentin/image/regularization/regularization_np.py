7bd2fdc5745a10fb33bd97c136aa35ed84906c68
Vinschers <code@a.vicentin.xyz>
2026-02-10 11:35:13 +0100

    fix(image-regularization): fix gradient computation

--- src/vicentin/image/regularization/regularization_np.py
+++ src/vicentin/image/regularization/regularization_np.py
@@ -3,10 +3,10 @@ from vicentin.image.differentiation import grad
 
 
 def tychonov(img):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return np.sum(dx**2 + dy**2)
 
 
 def total_variation(img, epsilon=1e-2):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return np.sum(np.sqrt(epsilon**2 + dx**2 + dy**2))
--- src/vicentin/image/regularization/regularization_torch.py
+++ src/vicentin/image/regularization/regularization_torch.py
@@ -3,10 +3,10 @@ from vicentin.image.differentiation import grad
 
 
 def tychonov(img):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return torch.sum(dx**2 + dy**2)
 
 
 def total_variation(img, epsilon=1e-2):
-    dx, dy = grad(img)
+    dx, dy = grad(img, mode="forward", boundary="wrap")
     return torch.sum(torch.sqrt(epsilon**2 + dx**2 + dy**2))
